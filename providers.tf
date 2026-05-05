e9eb4bea3cdea48f0ace28b823036c407d94e46b
sandy <pandeysandip35@gmail.com>
2026-05-05 15:20:49 +0530

    Added Azure backend configuration for Terraform state

--- providers.tf
+++ providers.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "backend-rg"
+    storage_account_name = "testprojectstg01"
+    container_name       = "tfstate"
+    key                  = "test.tfstate"
+  }
+}
+
 provider "azurerm" {
     features {}
     subscription_id = "01f21eee-bb54-414a-be2d-2c52eb70a68c"
